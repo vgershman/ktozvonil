f499ab174de3643ab7442dd254bca6a208ee7e1f
vgershman <herrschmann@gmail.com>
2013-01-06 01:18:50 +0400

    1 commit

---
+++ src/com/vgershman/ktozvonil/connection/RequestTask.java
@@ -0,0 +1,11 @@
+package com.vgershman.ktozvonil.connection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vgershman
+ * Date: 06.01.13
+ * Time: 0:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestTask {
+}
